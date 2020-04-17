8f141573785cb9819f61c743ed22dd5e78b35a43
Clypeus <Clypeus.infection@gmail.com>
2020-04-17 20:47:47 +0100

    Create a SSIS Package to import the data into the organism tables.
    Also creates a little script to clean those tables for when we want to start this process again.

---
+++ MAIN/Clypeus/S2 - Cleaning Up The Organism Support Tables.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM [medicinals].[OrganismClass]
+DBCC CHECKIDENT ('[medicinals].[OrganismClass]', RESEED, 1)
+
+DELETE FROM [medicinals].[OrganismFamily]
+DBCC CHECKIDENT ('[medicinals].[OrganismFamily]', RESEED, 1)
+
+DELETE FROM [medicinals].[OrganismKingdom]
+DBCC CHECKIDENT ('[medicinals].[OrganismKingdom]', RESEED, 1)
+
+DELETE FROM [medicinals].[OrganismOrder]
+DBCC CHECKIDENT ('[medicinals].[OrganismOrder]', RESEED, 1)
+
+DELETE FROM [medicinals].[OrganismPhylum]
+DBCC CHECKIDENT ('[medicinals].[OrganismPhylum]', RESEED, 1)
+
+DELETE FROM [medicinals].[OrganismTypes]
+DBCC CHECKIDENT ('[medicinals].[OrganismTypes]', RESEED, 1)
