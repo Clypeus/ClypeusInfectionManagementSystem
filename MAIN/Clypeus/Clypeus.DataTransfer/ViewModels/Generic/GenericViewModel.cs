3b08bb91bdcda89720ac32ff69b07ffe812806b9
Clypeus <Clypeus.infection@gmail.com>
2020-04-18 18:16:08 +0100

    Started to work on some practical implementions of a service.

---
+++ MAIN/Clypeus/Clypeus.DataTransfer/ViewModels/Generic/GenericViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.DataTransfer.ViewModels.Generic
+{
+    public class GenericViewModel
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
---
+++ MAIN/Clypeus/Clypeus.Services/Interfaces/Medicinals/IDrugService.cs
@@ -0,0 +1,12 @@
+﻿using Clypeus.DataTransfer.ViewModels.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.Services.Interfaces.Medicinals
+{
+    public interface IDrugService
+    {
+        IEnumerable<GenericViewModel> GetDrugs();
+    }
+}
