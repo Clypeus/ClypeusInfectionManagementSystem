f5f1aba7636ad3ab6cf2ac56c55915e128d491a8
Clypeus <Clypeus.infection@gmail.com>
2020-04-19 17:33:25 +0100

    Added an outline of the paging code, this is taken mainly from gunnar peipmans implementions.

---
+++ MAIN/Clypeus/Clypeus.Web.Components/Paging.razor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Clypeus.Web.Components.Support;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+//This is based on the code provided by Gunner Piepman online at: https://gunnarpeipman.com/
+
+namespace Clypeus.Web.Components
+{
+    public class PagerModel : ComponentBase
+    {
+        [Parameter]
+        public PagedResultBase Result { get; set; }
+
+        [Parameter]
+        public Action<int> PageChanged { get; set; }
+
+        protected int StartIndex { get; private set; } = 0;
+        protected int FinishIndex { get; private set; } = 0;
+
+        protected override void OnParametersSet()
+        {
+            StartIndex = Math.Max(Result.CurrentPage - 5, 1);
+            FinishIndex = Math.Min(Result.CurrentPage + 5, Result.PageCount);
+
+            base.OnParametersSet();
+        }
+
+        protected void PagerButtonClicked(int page)
+        {
+            PageChanged?.Invoke(page);
+        }
+    }
+}
---
+++ MAIN/Clypeus/Clypeus.Web.Components/Support/PagedResultBase.cs
@@ -0,0 +1,27 @@
+﻿//This is based on the code provided by Gunner Piepman online at: https://gunnarpeipman.com/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.Web.Components.Support
+{
+    public abstract class PagedResultBase
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+
+        public int FirstRowOnPage
+        {
+
+            get { return (CurrentPage - 1) * PageSize + 1; }
+        }
+
+        public int LastRowOnPage
+        {
+            get { return Math.Min(CurrentPage * PageSize, RowCount); }
+        }
+    }
+}
