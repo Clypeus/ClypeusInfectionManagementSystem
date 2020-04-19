8fdf622ad230838f30e5a7cca5fe58047d1cdb0e
Clypeus <Clypeus.infection@gmail.com>
2020-04-19 18:41:17 +0100

    Updated the pagination model, so that it more closey fits how i see this going moving forward, still clear;y but seperates the logic, its more like a  angular component.

--- MAIN/Clypeus/Clypeus.Web.Components/Paging.razor.cs
+++ MAIN/Clypeus/Clypeus.Web.Components/Paging.razor.cs
@@ -12,19 +12,33 @@ namespace Clypeus.Web.Components
 {
     public class PagerModel : ComponentBase
     {
+        
         [Parameter]
-        public PagedResultBase Result { get; set; }
+        public int? NumberOfResults { get; set; }
+
+        [Parameter]
+        public int? ResultsPerPage { get; set;}
 
         [Parameter]
         public Action<int> PageChanged { get; set; }
 
+        [Parameter]
+        public int CurrentPage { get; set; } = 1;
+
         protected int StartIndex { get; private set; } = 0;
         protected int FinishIndex { get; private set; } = 0;
 
+        protected int PageCount =0;
+
         protected override void OnParametersSet()
         {
-            StartIndex = Math.Max(Result.CurrentPage - 5, 1);
-            FinishIndex = Math.Min(Result.CurrentPage + 5, Result.PageCount);
+            if (NumberOfResults.HasValue && ResultsPerPage.HasValue)
+            {
+                PageCount = (int)Math.Ceiling((double)NumberOfResults.Value / ResultsPerPage.Value);
+            }
+
+            StartIndex = Math.Max(CurrentPage - 5, 1);
+            FinishIndex = Math.Min(CurrentPage + 5,PageCount);
 
             base.OnParametersSet();
         }
