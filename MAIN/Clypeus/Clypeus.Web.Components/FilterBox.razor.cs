9f4b419ecc7455b3a4734bd8e8495a357e9917e5
Clypeus <Clypeus.infection@gmail.com>
2020-04-22 20:52:53 +0100

    Created a filterbox control, and started to do some research on how a debounce would be implemented

---
+++ MAIN/Clypeus/Clypeus.Web.Components/FilterBox.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Clypeus.Web.Components
+{
+    public class FilterBoxModel:ComponentBase
+    {
+        [Parameter]
+        public int Debounce { get; set; } = 1000;
+
+        [Parameter]
+        public string FilterString { get; set; }
+
+        [Parameter]
+        public Action<string> FilterTextChanged { get; set; }
+
+        [Parameter]
+        public Action FilterCleared { get; set; }
+    }
+}
