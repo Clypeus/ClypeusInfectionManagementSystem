70140d989f11c4d29f57d1ced8931125b68217dc
Clypeus <Clypeus.infection@gmail.com>
2020-04-22 21:12:42 +0100

    Implemented the debounce functionality

--- MAIN/Clypeus/Clypeus.Web.Components/FilterBox.razor.cs
+++ MAIN/Clypeus/Clypeus.Web.Components/FilterBox.razor.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Timers;
 
 namespace Clypeus.Web.Components
 {
     public class FilterBoxModel:ComponentBase
     {
+        private System.Timers.Timer debounceTimer;
+
         [Parameter]
         public int Debounce { get; set; } = 1000;
 
@@ -19,5 +23,26 @@ namespace Clypeus.Web.Components
 
         [Parameter]
         public Action FilterCleared { get; set; }
+
+        protected override void OnInitialized()
+        {
+            debounceTimer = new System.Timers.Timer(Debounce);
+            debounceTimer.Elapsed += OnTextInputFinished;
+            debounceTimer.AutoReset = false;
+        }
+
+        protected void HandleKeyUp(KeyboardEventArgs e)
+        {
+            debounceTimer.Stop();
+            debounceTimer.Start();
+        }
+
+        private void OnTextInputFinished(Object source, ElapsedEventArgs e)
+        {
+            InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+        }
     }
 }
