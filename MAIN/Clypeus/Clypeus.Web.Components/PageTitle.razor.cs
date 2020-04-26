4b783c484f740fffa6dac2ad4dd260c26ec90a08
Clypeus <Clypeus.infection@gmail.com>
2020-04-26 15:31:07 +0100

    Updated the css so that i reflects on how life was before the inversion of the links.

---
+++ MAIN/Clypeus/Clypeus.Web.Components/PageTitle.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.Web.Components
+{
+    public class PageTitleModel : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+    }
+}
