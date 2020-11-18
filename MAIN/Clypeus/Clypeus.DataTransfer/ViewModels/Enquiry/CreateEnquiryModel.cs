66a24d87f1978bab88920774a5eff709ad96c765
Clypeus <Clypeus.infection@gmail.com>
2020-11-18 19:51:32 +0000

    Started to create a new view model

---
+++ MAIN/Clypeus/Clypeus.DataTransfer/ViewModels/Enquiry/CreateEnquiryModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clypeus.DataTransfer.ViewModels.Enquiry
+{
+    public class CreateEnquiryModel
+    {
+        public DateTime? EnquiryDateAndTime { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "Familyname is too long.")]
+        public string Surname {get;set;}
+
+        [Required]
+        [StringLength(35, ErrorMessage = "Forename is too long.")]
+        public string Forename { get; set; }
+
+        [Required]
+        [StringLength(125, ErrorMessage = "Description is too long.")]
+        public string Description { get; set; }
+
+        public int LocationId { get; set; }
+
+        [Required]
+        [StringLength(125, ErrorMessage = "Location is too long.")]
+        public string LocationDescription { get; set; }
+
+
+    }
+}
