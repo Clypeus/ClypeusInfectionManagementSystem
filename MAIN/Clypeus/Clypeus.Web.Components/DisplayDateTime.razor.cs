37c3a0fb4b66a051b8735ad5fa936a56f9e318ed
Clypeus <Clypeus.infection@gmail.com>
2020-11-18 21:05:21 +0000

    Adds a new component to display the date time.

---
+++ MAIN/Clypeus/Clypeus.Web.Components/DisplayDateTime.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.Web.Components
+{
+    public class DisplayDateTimeModel : ComponentBase
+    {
+        [Parameter]
+        public string DisplayValue { get; set; }
+
+        [Parameter]
+        public DateTime RawValue { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        protected override void OnInitialized()
+        {
+            RawValue = DateTime.Now;
+            DisplayValue = string.Format("{0} {1}",Title,RawValue.ToString());
+        }
+    }
+}
