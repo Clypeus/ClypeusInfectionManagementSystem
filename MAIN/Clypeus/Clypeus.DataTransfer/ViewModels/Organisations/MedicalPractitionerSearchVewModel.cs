aad9c3c3aa447775d3e1e5fca47f54c609e3d117
Clypeus <Clypeus.infection@gmail.com>
2020-12-20 17:16:29 +0000

    Starts to implement a list system for practioners, complete with search.

---
+++ MAIN/Clypeus/Clypeus.DataTransfer/ViewModels/Organisations/MedicalPractitionerSearchVewModel.cs
@@ -0,0 +1,16 @@
+﻿using Clypeus.DataTransfer.ViewModels.People;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clypeus.DataTransfer.ViewModels.Organisations
+{
+    public class MedicalPractitionerSearchVewModel
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public int PracticeId { get; set; }
+    }
+}
---
+++ MAIN/Clypeus/Clypeus/Pages/ReferenceData/MedicalPractitionerList.razor.cs
@@ -0,0 +1,31 @@
+﻿using Clypeus.DataTransfer.ViewModels.Organisations;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clypeus.Pages.ReferenceData
+{
+    public class MedicalPractitionerListModel:ComponentBase
+    {
+        public MedicalPractitionerSearchVewModel searchModel = new MedicalPractitionerSearchVewModel();
+
+        public void HandleValidSubmit()
+        {
+            
+        }
+
+        public void Search()
+        {
+
+        }
+
+        public void ClearForm()
+        {
+            searchModel.Code = null;
+            searchModel.Name = null;
+        }
+
+    }
+}
